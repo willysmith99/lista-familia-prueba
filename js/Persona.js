8fa8fc30c6bcfaacad5592c13c015adeec9699a7
willyn smith <smith99willyn@gmail.com>
2022-08-26 11:54:21 -0500

    mi primer commit

---
+++ js/Persona.js
@@ -0,0 +1,24 @@
+
+
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    getNombre(){
+        return this.nombre;
+    }
+
+    setNombre(nombre){
+        this.nombre = nombre;
+    }
+
+    getApellido(){
+        return this.apellido;
+    }
+
+    setApellido(apellido){
+        this.apellido = apellido;
+    }
+    
+}
